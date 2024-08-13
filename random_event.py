665d17bbe8f3a7d50f7eadcee462d42a902deaa0
gouherdanishiitkgp@gmail.com <gouherdanishiitkgp@gmail.com>
2024-08-13 16:45:02 +0530

    Modified Random Event to include repr

--- random_event.py
+++ random_event.py
@@ -1,16 +1,19 @@
 import numpy as np
 
 class RandomEvent:
-    def __init__(self,x,px) -> None:
+    def __init__(self,x=0,prob=0) -> None:
         self.x = x
-        self.px = px
+        self.prob = prob
 
     def __str__(self) -> str:
-        return f'Event(x={self.x}, p(x)={self.px})'
+        return f'Event(x={self.x}, p(x)={self.prob:.2f})'
+    
+    def __repr__(self) -> str:
+        return str(self)
 
     @property
     def information(self):
         """
         How much surprise one gets when a specific event happens
         """
-        return -np.log(self.px)
+        return -np.log(self.prob)
